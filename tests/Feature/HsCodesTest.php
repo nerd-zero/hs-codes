c1d3a60e4acaec61d5068330dd06a775239c80e1
mqoe <mqoeyyy@gmail.com>
2025-08-01 09:30:23 +0200

    feat: use factory pattern to create the HSCodes class

--- tests/Feature/HsCodesTest.php
+++ tests/Feature/HsCodesTest.php
@@ -1,42 +1,39 @@
 <?php
 
+use NerdZero\HsCodes\HsCodeItem;
 use NerdZero\HsCodes\HsCodes;
 
-beforeEach(function () {
-    $testFile = __DIR__ . '/data/hs_codes_fake.csv';
-    HsCodes::setDataFile($testFile);
-});
-
 it('loads data from CSV file', function () {
-    $codes = HsCodes::getCodes();
-
-    var_dump($codes);
+    $codes = new HsCodes(__DIR__ . '/data/hs_codes_fake.csv')->getCodes();
     expect($codes)->toBeArray()->toHaveCount(2);
 });
 
 it('correctly gets description by HS code', function () {
-    expect(HsCodes::getDescription('12345'))->toBe('Test 1')
-        ->and(HsCodes::getDescription('56789'))->toBe('Test 2')
-        ->and(HsCodes::getDescription('99999'))->toBeNull();
+    $hsCodes = new HsCodes(__DIR__ . '/data/hs_codes_fake.csv');
+
+    printf($hsCodes->getDescription('12345'));
+
+    expect($hsCodes->getDescription('12345'))->toBe('Test 1')
+        ->and($hsCodes->getDescription('56789'))->toBe('Test 2')
+        ->and($hsCodes->getDescription('99999'))->toBeNull();
 });
 
 it('returns all descriptions', function () {
-    $descriptions = HsCodes::getDescriptions();
+    $descriptions = new HsCodes(__DIR__ . '/data/hs_codes_fake.csv')->getDescriptions();
     expect($descriptions)->toBeArray()->toHaveCount(2)
         ->toContain('Test 1')
         ->toContain('Test 2');
 });
 
-it('returns complete lookup array', function () {
-    $lookup = HsCodes::getLookup();
+it('returns complete lookup array with HsCodeItem objects', function () {
+    $lookup = new HsCodes(__DIR__ . '/data/hs_codes_fake.csv')->getLookup();
     expect($lookup)->toBeArray()->toHaveCount(2)
-        ->toMatchArray([
-            '12345' => 'Test 1',
-            '56789' => 'Test 2'
-        ]);
+        ->and($lookup['12345'])->toBeInstanceOf(HsCodeItem::class)
+        ->and($lookup['56789'])->toBeInstanceOf(HsCodeItem::class)
+        ->and($lookup['12345']->description)->toBe('Test 1')
+        ->and($lookup['56789']->description)->toBe('Test 2');
 });
 
 it('throws exception for missing file', function () {
-    HsCodes::setDataFile('nonexistent_file.csv');
-    HsCodes::getCodes();
+    new HsCodes('nonexistent_file.csv')->getCodes();
 })->throws(RuntimeException::class);
